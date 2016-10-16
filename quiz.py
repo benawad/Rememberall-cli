5e53b3a733b298268dc6e19946d45f3a7e03e957
ben awad <benawad97@gmail.com>
2016-10-16 09:42:48 -0500

    search

--- quiz.py
+++ quiz.py
@@ -4,6 +4,21 @@ import json
 
 command = sys.argv[1]
 
+def search_quizlet(q):
+    client_id = "anpmmF7bXs"
+    payload = {'client_id': client_id, 'whitespace': 1}
+    r = requests.get("https://api.quizlet.com/2.0/search/sets?q=%s" % q, params=payload)
+
+    data = json.loads(r.text)
+    # list_thumbnails(recipient_id, list(map(set_to_element, data['sets'][:5])))
+    print("Select a number to quiz:")
+    for i, s in enumerate(data['sets'][:5]):
+        print("%s %s by %s" % (i, s['title'], s['created_by']))
+
+    n = int(input())
+    sys.argv[2] = data['sets'][n]['id']
+
+
 def fetch_quizlet(deck_id):
     client_id = "anpmmF7bXs"
     payload = {'client_id': client_id, 'whitespace': 1}
@@ -22,6 +37,10 @@ def fetch_quizlet(deck_id):
         'cards': cards,
     }
 
+
+if command == "search":
+    search_quizlet(sys.argv[2])
+    command = "quiz"
 
 if command == "quiz":
     qid = sys.argv[2]
